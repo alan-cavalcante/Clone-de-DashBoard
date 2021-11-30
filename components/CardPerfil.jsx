99172abc7a58d9413dfcbe34c246c0c493109791
alan-cavalcante <aocaval@gmail.com>
2021-11-30 19:23:31 -0300

    CSS 90%, falta media query e integrar api

---
+++ components/CardPerfil.jsx
@@ -0,0 +1,29 @@
+import React from 'react'
+import Image from 'next/image'
+
+
+export default function CardPerfil(props) {
+    return (
+        <div className="bg-azul-darkBlue rounded-t-lg rounded-lg row-span-2 flex flex-col">
+            <div className="self-center text-size70 bg-azul-base h-4/6 rounded-lg text-white font-rubik p-6 flex flex-col items-start">
+
+                <div className="rounded-full border-2 border-white overflow-hidden flex">
+                    <Image src="/setenta.jpg" height={75} width={75} layout="fixed" />
+                </div>
+
+                <p className="text-xs font-thin mt-10">
+                    Report for
+                </p>
+                <div className="text-3xl font-extralight">
+                    {props.nome} {props.sobrenome}
+                </div>
+            </div>
+            
+            <div className="p-6 flex flex-col text-left items-start justify-between text-azul-desaturado font-rubik font-thin h-2/6">
+                <button className="hover:text-white" >Daily</button>
+                <button className="hover:text-white" >Weekly</button>
+                <button className="hover:text-white" >Monthly</button>
+            </div>
+        </div>
+    )
+}
--- components/Main.jsx
+++ components/Main.jsx
@@ -1,16 +1,16 @@
 import Card from "./Card"
-
+import CardPerfil from "./CardPerfil"
 function Main() {
     return (
         <div className=" bg-azul-veryDarkBlue h-screen w-screen flex items-center justify-center">
-            <div className="h-3/5 w-auto rounded-lg grid grid-cols-4 gap-8">
-                <div className="rounded-lg bg-gray-500 row-span-2"></div>
-                <Card periodo="Periodo" previousTime="10hrs" current="32hrs"nome="Work"/>
-                <Card periodo="Periodo" previousTime="10hrs" current="32hrs"nome="Play"/>
-                <Card periodo="Periodo" previousTime="10hrs" current="32hrs"nome="Study"/>
-                <Card periodo="Periodo" previousTime="10hrs" current="32hrs"nome="Exercise"/>
-                <Card periodo="Periodo" previousTime="10hrs" current="32hrs"nome="Social"/>
-                <Card periodo="Periodo" previousTime="10hrs" current="32hrs"nome="Self Care"/>
+            <div className="h-3/6 w-auto rounded-lg grid grid-cols-4 gap-8">
+                <CardPerfil nome="Alfredo" sobrenome="Neto"/>
+                <Card periodo="Periodo" previousTime="10hrs" current="32hrs" nome="Work" />
+                <Card periodo="Periodo" previousTime="10hrs" current="32hrs" nome="Play" />
+                <Card periodo="Periodo" previousTime="10hrs" current="32hrs" nome="Study" />
+                <Card periodo="Periodo" previousTime="10hrs" current="32hrs" nome="Exercise" />
+                <Card periodo="Periodo" previousTime="10hrs" current="32hrs" nome="Social" />
+                <Card periodo="Periodo" previousTime="10hrs" current="32hrs" nome="Self Care" />
             </div>
         </div>
     )
